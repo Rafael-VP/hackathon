290254ee00186dcb3c410604faf022ef10feae23
Batathi <lfnadalinimarafon@gmail.com>
2024-10-28 11:52:58 -0300

    Adicionado conversão OGG para WAV

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import configparser
 import speech_recognition as sr
 from openai import OpenAI
+from pydub import AudioSegment
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -16,11 +17,18 @@ st.title("Ordem de serviço:")
 uploaded_files = st.file_uploader("Arquivos de áudio:", type=['ogg', 'wav'], accept_multiple_files=True)
 
 for uploaded_file in uploaded_files:
+        # Salvando arquivo ogg
         path = os.path.join("userdata", uploaded_file.name)
         with open(path, "wb") as f:
                 f.write(uploaded_file.getvalue())
         print(path)
 
+        # Convertendo arquivo para WAV
+        audio = AudioSegment.from_ogg(path)
+        path = path.replace("ogg", "wav")
+        audio.export(path, format="wav")
+
+        # Convertendo áudio para texto
         file_audio = sr.AudioFile(path)
 
         with file_audio as source:
@@ -39,6 +47,5 @@ for uploaded_file in uploaded_files:
             ]
         )
 
-        st.write(response.choices[0].message.contentt)
+        st.write(response.choices[0].message.content)
 
-#Tixinha SIGMA
